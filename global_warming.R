0bce0e7640540829919c9bd0dde194a304b088bf
Hammad Safi <zh25aba@herts.ac.uk>
2025-11-27 17:29:09 +0000

    Removed F charecter from year using script

--- global_warming.R
+++ global_warming.R
@@ -1,1 +1,4 @@
 #R Code will be written here
+library(tidyverse)
+global_Warming_dataset <- read_csv('long_format_annual_surface_temp.csv',) #Importing Dataset
+global_Warming_dataset$Year <- sapply(global_Warming_dataset$Year, function(x) gsub("F", "",  x)) #Removing F character from years

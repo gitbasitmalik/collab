8aa2e57249eb05a7c4a9a0213bba39198ad8312f
Hammad Safi <zh25aba@herts.ac.uk>
2025-11-28 17:53:26 +0000

    Grouped the dataset based on year and the average temperature

--- global_warming.R
+++ global_warming.R
@@ -1,4 +1,17 @@
-#R Code will be written here
+#loading and arranging the dataset
 library(tidyverse)
 global_Warming_dataset <- read_csv('long_format_annual_surface_temp.csv',) #Importing Dataset
 global_Warming_dataset$Year <- sapply(global_Warming_dataset$Year, function(x) gsub("F", "",  x)) #Removing F character from years
+global_Warming_dataset$Year <- as.numeric(global_Warming_dataset$Year) #Coverting Year to numeric type
+
+#Grouping the dataset based upon the years and mean of the temperature in whole world in each year
+yearly_global_mean <- global_Warming_dataset %>% #Creating a new subset of Main dataset
+  +     group_by(Year) %>% #and then grouping the data by Year
+  +     summarise(global_Temperature = mean(Temperature, na.rm = TRUE),) %>% #and then calculating the mean of the global temperature for each year
+  +     mutate(global_Temperature = round(global_Temperature, 4)) # and then rounding off the result upto 4 decimal points
+
+#Visualizing the Data
+plot(yearly_global_mean$Year, yearly_global_mean$global_Temperature, # Scatter plotting the change in global temperature with year
+     main = "Global Temperature vs Year",xlab = "Year", ylab = "Global Temperature", 
+     sub = "Is there correlartion between the Global Temperature and Years from 1961 to 2022") 
+
